2a5760a8ffa879dc9057fc7b195e68a0568bba54
miryarik <85996187+miryarik@users.noreply.github.com>
2025-03-09 16:51:24 +0530

    added function to hit api for weather info

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,42 @@
 import "../styles/modern-reset.css";
 import "../styles/styles.css";
 
+const API_KEY = "7KKXH6MLCCV3XZGMGVHSL6NUR";
 
-console.log("Ready");
+let location = "srinagar, kashmir";
+
+async function getWeatherInfo(location) {
+    const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API_KEY}&contentType=json`,
+        { mode: "cors" }
+    );
+    const data = await response.json();
+
+    // console.log(data)
+
+    const weatherInfo = {
+        "location" : data.resolvedAddress,
+        "description": data.description,
+        "timezone": data.timezone,
+        "current" :  {
+            "condition" : data.currentConditions.conditions,
+            "time": data.currentConditions.datetime,
+            "temperature": data.currentConditions.temp,
+            "feelslike": data.currentConditions.feelslike,
+            "humidity": data.currentConditions.humidity,
+            "precip": data.currentConditions.precip,
+            "precipprob": data.currentConditions.precipprob,
+            "preciptype": data.currentConditions.preciptype,
+            "uvindex": data.currentConditions.uvindex,
+            "visibility": data.currentConditions.visibility,
+            "winddir": data.currentConditions.winddir,
+            "windspeed": data.currentConditions.windspeed,
+        }
+    }
+
+    return weatherInfo;
+}
+
+const info = await getWeatherInfo(location);
+console.log(info);
+
